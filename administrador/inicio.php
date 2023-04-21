481c2b4bb34c9b79405c2ef73efb49dc0ce2e6ea
Josep Valls <alfredvays@gmail.com>
2023-04-21 08:38:56 +0200

    Admin zone ok  Changes to be committed: 	modified:   administrador/inicio.php

--- administrador/inicio.php
+++ administrador/inicio.php
@@ -1,5 +1,13 @@
 <?php include('template/header.php') ?>
-            <div class="col-md-12">
-                
+            <div class="col-md-12 mt-5">
+                <div class="jumbotron">
+                    <h1 class="display-3">Welcome <?php echo $userName ?></h1>
+                    <p class="lead">Bibliotheque Admin</p>
+                    <hr class="my-2">
+                    <p>More info</p>
+                    <p class="lead">
+                        <a class="btn btn-primary btn-lg" href="sections/books.php" role="button">Go to books admin</a>
+                    </p>
+                </div>
             </div>          
 <?php include('template/footer.php') ?>
